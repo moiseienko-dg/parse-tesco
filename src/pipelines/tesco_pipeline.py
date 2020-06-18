863c8dbccbde896d59b546b09dadb4969b82a7a3
moiseienko-dg <moiseienko.dg@gmail.com>
2020-06-18 10:52:26 +0300

    [fix] export items

--- src/pipelines/tesco_pipeline.py
+++ src/pipelines/tesco_pipeline.py
@@ -1,14 +1,19 @@
-import json
+from scrapy.exporters import JsonItemExporter
 
-class TescoPipeline:
+
+class TescoPipeline(object):
+
+    file = None
 
     def open_spider(self, spider):
-        self.file = open('items.jl', 'w')
+        self.file = open('item.json', 'w')
+        self.exporter = JsonItemExporter(self.file)
+        self.exporter.start_exporting()
 
     def close_spider(self, spider):
+        self.exporter.finish_exporting()
         self.file.close()
 
     def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + "\n"
-        self.file.write(line)
+        self.exporter.export_item(item)
         return item
